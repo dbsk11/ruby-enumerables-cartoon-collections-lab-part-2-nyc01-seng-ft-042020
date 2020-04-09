ec39511ec52abb578e74a28c8c839127a6c5017b
Jessica K <jessikim807@gmail.com>
2020-04-09 14:56:37 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,6 @@
 def square_array(array)
   array.map do |squares|
-    squares ** squares
+    squares ** 2
   end
   # Use an Enumerable to square every element in the passed in array
   # Return a new array of the results
